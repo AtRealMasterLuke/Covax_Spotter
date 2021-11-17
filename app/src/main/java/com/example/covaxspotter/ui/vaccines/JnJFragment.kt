2a4da54f68d10f8740756b354bc09a831f688a2a
dennisk748 <kamaugitonga748@gmail.com>
2021-11-17 08:36:21 +0300

    JnJ Modification

--- app/src/main/java/com/example/covaxspotter/ui/vaccines/JnJFragment.kt
+++ app/src/main/java/com/example/covaxspotter/ui/vaccines/JnJFragment.kt
@@ -5,20 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.covaxspotter.R
+import com.example.covaxspotter.databinding.FragmentJnJBinding
+import com.example.covaxspotter.utils.johnsons
 
 
-/**
- * TODOS
- * Add binding object and inflate the layout using the binding object.
- */
 class JnJFragment : Fragment() {
+    private var _binding: FragmentJnJBinding?= null // backing property
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jn_j, container, false)
+        _binding = FragmentJnJBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.JnJText.text = johnsons
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null //release memory at the end of lifecycle
     }
 }
